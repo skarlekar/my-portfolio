95eef8f4bb64a65b1caec5494d66dc0141cb0859
Srini Karlekar <skarlekar@yahoo.com>
2017-09-17 14:06:38 -0400

    Updated upload Portfolio Python code added

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -3,15 +3,19 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
-portfolio_bucket = s3.Bucket('portfolio.karlekar.com')
-build_bucket = s3.Bucket('portfoliobuild.karlekar.com')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
+    portfolio_bucket = s3.Bucket('portfolio.karlekar.com')
+    build_bucket = s3.Bucket('portfoliobuild.karlekar.com')
 
-portfolio_buffer = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_buffer)
+    portfolio_buffer = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_buffer)
 
-with zipfile.ZipFile(portfolio_buffer) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    with zipfile.ZipFile(portfolio_buffer) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    print "Code pushed!"
+    return 'Code Pushed by Lambda'
