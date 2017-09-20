f1c5595b32d7c98e133f4e5d32d4a15a2e10efc3
Srini Karlekar <skarlekar@yahoo.com>
2017-09-20 13:45:34 -0400

    Lambda function updated to read bucket name and key from CodePipeline job as well as manual build job

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -2,17 +2,29 @@ import boto3
 import StringIO
 import zipfile
 import mimetypes
+import json
 
 s3 = boto3.resource('s3')
 portfolio_bucket = s3.Bucket('portfolio.karlekar.com')
 build_bucket = s3.Bucket('portfoliobuild.karlekar.com')
+build_bucket_key = 'portfoliobuild.zip'
 sns = boto3.resource('sns')
 topic = sns.Topic('arn:aws:sns:us-east-1:219104658389:deployPortfolioTopic')
 
 def lambda_handler(event, context):
     try:
+        job = event.get("CodePipeline.job")
+        print "job = "+json.dumps(job)
+        if job:
+            input_artifact = job["data"]["inputArtifacts"][0]
+            print "Input Artifact = " + str(input_artifact)
+            location = input_artifact["location"]["s3Location"]
+            print "Location = " + str(location)
+            build_bucket = s3.Bucket(location["bucketName"])
+            build_bucket_key = location["objectKey"]
+
         portfolio_buffer = StringIO.StringIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_buffer)
+        build_bucket.download_fileobj(build_bucket_key, portfolio_buffer)
 
         with zipfile.ZipFile(portfolio_buffer) as myzip:
             for nm in myzip.namelist():
@@ -23,4 +35,5 @@ def lambda_handler(event, context):
         topic.publish(Subject="Portfolio code pushed successfully!", Message="Portfolio code pushed from portfoliobuild.karlekar.com to portfolio.karlekar.com")
     except:
         topic.publish(Subject="Portfolio code push failed!", Message="Portfolio code push from portfoliobuild.karlekar.com to portfolio.karlekar.com failed. Please review the logs.")
-    return 'Portfolio Lambda Completed'
+        raise
+    return 'Portfolio Lambda Completed Successfully'
